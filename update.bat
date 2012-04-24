13de85709dbbc0eff3905cd601a66001fed251fd
unknown <dariosky@.MION.COM>
2012-04-24 11:32:50 +0200

    Aggiungo i file .bat per lo sviluppo in Windows

---
+++ update.bat
@@ -0,0 +1,16 @@
+@echo off
+call ..\environment\Scripts\activate.bat
+set DJANGO_SETTINGS_MODULE=settings
+
+rem Migro i DB all'ultima versione
+python manage.py migrate
+
+python manage.py collectstatic --noinput
+python manage.py generatemedia
+
+python manage.py sqlitereindex tam
+python manage.py sqlitereindex fatturazione
+
+python manage.py cleanup
+
+echo Fine.
