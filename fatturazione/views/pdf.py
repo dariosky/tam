e7faeb3aedb83820f63f0b0a6a909bab8ad7241e
Dario Varotto <dariosky@gmail.com>
2012-05-13 21:19:26 +0200

    Le ricevute divise in due: ricevute consorzio e ricevute conducente. Cambio di template e numerazione per template.

--- fatturazione/views/pdf.py
+++ fatturazione/views/pdf.py
@@ -4,260 +4,205 @@ import os
 from django import http
 import copy
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import * #@UnusedWildImport
-from reportlab.pdfgen import canvas
+from reportlab.platypus import Frame, Paragraph, PageBreak, KeepTogether, TableStyle, Table, Preformatted #@UnusedImport
+from reportlab.platypus.doctemplate import BaseDocTemplate, NextPageTemplate, PageTemplate #@UnusedImport
+from reportlab.pdfgen import canvas #@UnusedImport
 #from reportlab.platypus import Image as FlowableImage
 from reportlab.lib.units import cm
-from reportlab.lib.pagesizes import A4, portrait, landscape
+from reportlab.lib.pagesizes import A4, portrait, landscape #@UnusedImport
 from reportlab.lib import colors
-from decimal import Decimal
 from django.templatetags.l10n import localize
+from fatturazione.views.money_util import moneyfmt, NumberedCanvas
 
-#def render_with_pisa(template_src, context_dict):
-#	import xhtml2pdf.pisa as pisa
-#	from django.template.loader import get_template
-#	from django.template.context import Context
-#	import StringIO
-#	import cgi
-#	template = get_template(template_src)
-#	context = Context(context_dict)
-#	html = template.render(context)
-#	result = StringIO.StringIO()
-#	pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("utf-8")), dest=result,
-#						link_callback=fetch_resources)
-#	if not pdf.err:
-#		return http.HttpResponse(result.getvalue(), mimetype='application/pdf')
-#	return http.HttpResponse('We had some errors<pre>%s</pre>' % cgi.escape(html))
+logoImage_path = os.path.join(settings.MEDIA_ROOT, settings.OWNER_LOGO)
+test = settings.DEBUG
 
-def fetch_resources(uri, rel):
-	path = os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
-	return path
+styles = getSampleStyleSheet()
+normalStyle = copy.deepcopy(styles['Normal'])
+normalStyle.fontSize = 8
+normalStyle.fontName = 'Helvetica'
 
 
+def onPage(canvas, doc, da, a):
+	width, height = canvas._doctemplate.pagesize
+	canvas.saveState()
+	stondata_style = ParagraphStyle("IntestazioneStondata", fontName='Helvetica', fontSize=8, leading=10,
+								 borderRadius=5, borderWidth=1, borderColor=colors.silver, borderPadding=5)
+	a_style = ParagraphStyle("Titolo della fattura", fontName='Helvetica', fontSize=8, leading=10)
+	tipo = doc.fattura.tipo
 
+	# set PDF properties ***************
+	canvas.setFont('Helvetica', 8)
+	canvas.setAuthor(settings.LICENSE_OWNER)
+	canvas.setCreator('TaM v.%s' % settings.TAM_VERSION)
+	canvas._doc.info.producer = ('TaM invoices')
+	canvas.setSubject(u"%s" % doc.fattura.nome_fattura())
+	if tipo == '3':
+		nome = 'Ricevuta'
+	else:
+		nome = 'Fattura'	# Fatture consorzio e conducente si chiamano semplicemente FATTURA	
+	descrittoreFattura = u"%s %s" % (nome, doc.fattura.descrittore())
+	canvas.setTitle(descrittoreFattura)
 
-def moneyfmt(value, places=2, curr='', sep='.', dp=',',
-			 pos='', neg='-', trailneg=''):
-	"""Convert Decimal to a money formatted string.
-	
-	places:  required number of places after the decimal point
-	curr:	optional currency symbol before the sign (may be blank)
-	sep:	 optional grouping separator (comma, period, space, or blank)
-	dp:	  decimal point indicator (comma or period)
-			 only specify as blank when places is zero
-	pos:	 optional sign for positive numbers: '+', space or blank
-	neg:	 optional sign for negative numbers: '-', '(', space or blank
-	trailneg:optional trailing minus indicator:  '-', ')', space or blank
-	
-	>>> d = Decimal('-1234567.8901')
-	>>> moneyfmt(d, curr='$')
-	'-$1,234,567.89'
-	>>> moneyfmt(d, places=0, sep='.', dp='', neg='', trailneg='-')
-	'1.234.568-'
-	>>> moneyfmt(d, curr='$', neg='(', trailneg=')')
-	'($1,234,567.89)'
-	>>> moneyfmt(Decimal(123456789), sep=' ')
-	'123 456 789.00'
-	>>> moneyfmt(Decimal('-0.02'), neg='<', trailneg='>')
-	'<0.02>'
-	
-	"""
-	q = Decimal(10) ** -places	  # 2 places --> '0.01'
-	sign, digits, exp = value.quantize(q).as_tuple()
-	result = []
-	digits = map(str, digits)
-	build, next = result.append, digits.pop
-	if sign:
-		build(trailneg)
-	for i in range(places):
-		build(next() if digits else '0')
-	build(dp)
-	if not digits:
-		build('0')
-	i = 0
-	while digits:
-		build(next())
-		i += 1
-		if i == 3 and digits:
-			i = 0
-			build(sep)
-	build(curr)
-	build(neg if sign else pos)
-	return ''.join(reversed(result))
-
-
-
-def render_to_reportlab(context):
-	fattura = context.get('fattura')
-	test = settings.DEBUG
-
-	class NumberedCanvas(canvas.Canvas):
-		def __init__(self, *args, **kwargs):
-			canvas.Canvas.__init__(self, *args, **kwargs)
-			self._saved_page_states = []
-
-		def showPage(self):
-			self._saved_page_states.append(dict(self.__dict__))
-			self._startPage()
-
-		def save(self):
-			"""add page info to each page (page x of y) if y>1"""
-			num_pages = len(self._saved_page_states)
-
-			for state in self._saved_page_states:
-				self.__dict__.update(state)
-				if num_pages > 1 or test:
-					self.draw_page_number(num_pages)
-				canvas.Canvas.showPage(self)
-
-			canvas.Canvas.save(self)
-
-		def draw_page_number(self, page_count):
-			self.setFont("Helvetica", 7)
-			self.drawRightString(width / 2, 1 * cm,
-				"Pagina %d di %d" % (self._pageNumber, page_count))
-
-	def firstPageTemplate(canvas, doc):
-		canvas.saveState()
-		stondata_style = ParagraphStyle("IntestazioneStondata", fontName='Helvetica', fontSize=8, leading=10,
-									 borderRadius=5, borderWidth=1, borderColor=colors.silver, borderPadding=5)
-		a_style = ParagraphStyle("Titolo della fattura", fontName='Helvetica', fontSize=8, leading=10)
-
-		# set PDF properties ***************
-		canvas.setFont('Helvetica', 8)
-		canvas.setAuthor(settings.LICENSE_OWNER)
-		canvas.setCreator('TaM v.%s' % settings.TAM_VERSION)
-		canvas._doc.info.producer = ('TaM invoices')
-		canvas.setSubject(u"%s" % fattura.nome_fattura())
-		if fattura.tipo == '3':
-			nome = 'Ricevuta'
-		else:
-			nome = 'Fattura'	# Fatture consorzio e conducente si chiamano semplicemente FATTURA	
-		descrittoreFattura = u"%s %s" % (nome, fattura.descrittore())
-		canvas.setTitle(descrittoreFattura)
-
-		# Header ***************
-		y = height - doc.topMargin
-		x = doc.leftMargin
-		if test:
-			canvas.setLineWidth(1)
-			p = canvas.beginPath()
-			p.moveTo(0, y); p.lineTo(width, y)
-			canvas.drawPath(p)
-		if fattura.tipo == "1":
-			logo_height = 2.5 * cm
-			y -= logo_height
-			canvas.drawImage(logoImage_path, x=x, y=y, width=7 * cm, height=logo_height)
-		descrittore = Paragraph('<font size="14"><b>%s</b></font> del %s' % (descrittoreFattura, localize(fattura.data)),
-								 a_style)
-		descrittore.wrapOn(canvas, width / 2, y)
-		descrittore.drawOn(canvas, x=x, y=y - descrittore.height)
-		y -= descrittore.height + 10
-
-		if fattura.tipo == '2':	# nelle fatture conducente metto il mittente a sinistra
-			fattura_da = Paragraph(fattura.emessa_da.strip().replace('\n', '<br/>'), a_style)
-			fattura_da.wrapOn(canvas, 6.5 * cm, 10 * cm)
-			fattura_da.drawOn(canvas, x, y - fattura_da.height)
-			y -= fattura_da.height
-			y -= 0.2 * cm	# spacer tra mittente e destinatario
-
-		if fattura.note:
-			note = Preformatted(fattura.note, a_style)
-			note.wrapOn(canvas, width / 2, 10 * cm)
-			y = y - note.height - 8
-			note.drawOn(canvas, 1 * cm, y=y)
-
-		if fattura.tipo in ("3"):
-			y = y - 10
-			testata_fissa = Paragraph("<font size='6'>Servizio trasporto emodializzato da Sua abitazione al centro emodialisi assistito e viceversa come da distinta.</font>", a_style)
-			testata_fissa.wrapOn(canvas, width / 2, 2 * cm)
-			y = y - testata_fissa.height
-			testata_fissa.drawOn(canvas, x, y)
-
-		left_y = y - 8	# spacer finale
-
-		if test:
-			p = canvas.beginPath()
-			p.moveTo(0, y); p.lineTo(width / 2, y)
-			canvas.drawPath(p)
-
-		# faccio la seconda colonna (destra) dell'header	
-		y = height - doc.topMargin
-		x = width - 8 * cm
-
-		if fattura.tipo <> '2':	# nelle fatture conducente metto il mittente a sinistra
-			fattura_da = Paragraph(fattura.emessa_da.strip().replace('\n', '<br/>'), a_style)
-			fattura_da.wrapOn(canvas, 6.5 * cm, 10 * cm)
-			fattura_da.drawOn(canvas, x, y - fattura_da.height)
-			y -= fattura_da.height
-			y -= 0.1 * cm	# spacer tra mittente e destinatario
-
-		fattura_a = Paragraph(fattura.emessa_a.replace('\n', '<br/>'), stondata_style)
-		fattura_a.wrapOn(canvas, 6.5 * cm, 10 * cm)
-		fattura_a.drawOn(canvas, x, y - fattura_a.height - fattura_a.style.borderPadding)
-
-		y -= fattura_a.height + fattura_a.style.borderPadding + 0.4 * cm # spazio finale
-		right_y = y
-		lower_y = min(left_y, right_y)
-
-		y = lower_y
-
-		if test:
-			p = canvas.beginPath()
-			p.moveTo(width / 2, y); p.lineTo(width, y)
-			canvas.drawPath(p)
-		note_finali.drawOn(canvas, doc.leftMargin, doc.bottomMargin)
-
-		# linea sotto l'intestazione
+	# Header ***************
+	y = height - doc.topMargin
+	x = doc.leftMargin
+	if test:
 		canvas.setLineWidth(1)
 		p = canvas.beginPath()
-		p.moveTo(doc.leftMargin, y)
-		p.lineTo(width - doc.rightMargin, y)
+		p.moveTo(0, y); p.lineTo(width, y)
 		canvas.drawPath(p)
+	if tipo == "1":
+		logo_height = 2.5 * cm
+		y -= logo_height
+		canvas.drawImage(logoImage_path, x=x, y=y, width=7 * cm, height=logo_height)
+	descrittore = Paragraph('<font size="14"><b>%s</b></font> del %s' % (descrittoreFattura, localize(doc.fattura.data)),
+							 a_style)
+	descrittore.wrapOn(canvas, width / 2, y)
+	descrittore.drawOn(canvas, x=x, y=y - descrittore.height)
+	y -= descrittore.height + 10
 
-		doc.pageTemplate.frames = [
-				Frame(doc.leftMargin, doc.bottomMargin + note_finali.height,
-					   width - (doc.leftMargin + doc.rightMargin), y - doc.bottomMargin - note_finali.height,
-					   showBoundary=test), #x,y, width, height
-			]
+	if tipo == '2':	# nelle fatture conducente metto il mittente a sinistra
+		fattura_da = Paragraph(da.strip().replace('\n', '<br/>'), a_style)
+		fattura_da.wrapOn(canvas, 6.5 * cm, 10 * cm)
+		fattura_da.drawOn(canvas, x, y - fattura_da.height)
+		y -= fattura_da.height
+		y -= 0.2 * cm	# spacer tra mittente e destinatario
 
-		canvas.setLineWidth(0.3)
+	if doc.fattura.note:
+		note = Preformatted(doc.fattura.note, a_style)
+		note.wrapOn(canvas, width / 2, 10 * cm)
+		y = y - note.height - 8
+		note.drawOn(canvas, 1 * cm, y=y)
+
+	if tipo in ("3"):
+		y = y - 10
+		testata_fissa = Paragraph("<font size='6'>Servizio trasporto emodializzato da Sua abitazione al centro emodialisi assistito e viceversa come da distinta.</font>", a_style)
+		testata_fissa.wrapOn(canvas, width / 2, 2 * cm)
+		y = y - testata_fissa.height
+		testata_fissa.drawOn(canvas, x, y)
+
+	left_y = y - 8	# spacer finale
+
+	if test:
 		p = canvas.beginPath()
-		p.moveTo(doc.leftMargin, doc.bottomMargin)
-		p.lineTo(width - doc.rightMargin, doc.bottomMargin)
+		p.moveTo(0, y); p.lineTo(width / 2, y)
 		canvas.drawPath(p)
 
-		canvas.restoreState()
+	# faccio la seconda colonna (destra) dell'header	
+	y = height - doc.topMargin
+	x = width - 8 * cm
 
+	if tipo <> '2':	# nelle fatture conducente metto il mittente a sinistra
+		fattura_da = Paragraph(da.strip().replace('\n', '<br/>'), a_style)
+		fattura_da.wrapOn(canvas, 6.5 * cm, 10 * cm)
+		fattura_da.drawOn(canvas, x, y - fattura_da.height)
+		y -= fattura_da.height
+		y -= 0.1 * cm	# spacer tra mittente e destinatario
 
-	response = http.HttpResponse(mimetype='application/pdf')
-	width, height = portrait(A4)
+	fattura_a = Paragraph(a.replace('\n', '<br/>'), stondata_style)
+	fattura_a.wrapOn(canvas, 6.5 * cm, 10 * cm)
+	fattura_a.drawOn(canvas, x, y - fattura_a.height - fattura_a.style.borderPadding)
 
-	doc = BaseDocTemplate(response,
-							pagesize=portrait(A4),
-							leftMargin=1 * cm,
-							rightMargin=1 * cm,
-							bottomMargin=1.5 * cm,
-							topMargin=1.5 * cm,
-							showBoundary=test,
-							pageTemplates=PageTemplate(onPage=firstPageTemplate),
-						)
+	y -= fattura_a.height + fattura_a.style.borderPadding * 2  # spazio finale	#TMP: era 2
+	right_y = y
+	lower_y = min(left_y, right_y)
 
+	y = lower_y
 
-
-	logoImage_path = os.path.join(settings.MEDIA_ROOT, settings.OWNER_LOGO)
-
-	styles = getSampleStyleSheet()
-	normalStyle = copy.deepcopy(styles['Normal'])
-	normalStyle.fontSize = 8
-	normalStyle.fontName = 'Helvetica'
+	if test:
+		p = canvas.beginPath()
+		p.moveTo(width / 2, y); p.lineTo(width, y)
+		canvas.drawPath(p)
 
 	note_finali_lines = []
-	for footer_row in context['invoices_footer']:
+	for footer_row in doc.fattura.footer:
 		note_finali_lines.append(footer_row)
 
 	note_finali = Paragraph("<br/>".join(note_finali_lines), normalStyle)
 	note_finali.wrap(width - doc.rightMargin - doc.leftMargin, 5 * cm)
+	note_finali.drawOn(canvas, doc.leftMargin, doc.bottomMargin)
+
+	# linea sotto l'intestazione
+	canvas.setLineWidth(1)
+	p = canvas.beginPath()
+	p.moveTo(doc.leftMargin, y)
+	p.lineTo(width - doc.rightMargin, y)
+	canvas.drawPath(p)
+
+	doc.pageTemplate.frames = [
+			Frame(doc.leftMargin, doc.bottomMargin + note_finali.height,
+				   width - (doc.leftMargin + doc.rightMargin), y - doc.bottomMargin - note_finali.height,
+				   showBoundary=test), #x,y, width, height
+		]
+
+	canvas.setLineWidth(0.3)
+	p = canvas.beginPath()
+	p.moveTo(doc.leftMargin, doc.bottomMargin)
+	p.lineTo(width - doc.rightMargin, doc.bottomMargin)
+	canvas.drawPath(p)
+
+	canvas.restoreState()
+
+def onPageNormal(canvas, doc):
+	#print "Normal"
+	onPage(canvas, doc, da=doc.fattura.emessa_da, a=doc.fattura.emessa_a)
+
+def onPageConducenteConsorzio(canvas, doc):
+	templateID = "ConducenteConsorzio"
+	currentTemplate = getattr(doc, 'lastTemplateID', doc.pageTemplates[0].id)
+	if currentTemplate <> templateID:
+		#print "Cambio da %s a %s" % (getattr(doc, 'lastTemplateID', None), templateID)
+		doc.lastTemplateID = templateID
+
+	#print "Conducente consorzio"
+	descrittore_fattura = doc.fattura.descrittore
+	doc.fattura.descrittore = lambda:"conducente"
+	onPage(canvas, doc, da=doc.fattura.emessa_da, a=settings.DATI_CONSORZIO)
+	doc.fattura.descrittore = descrittore_fattura
+
+def onPageConsorzioConducente(canvas, doc):
+	templateID = "ConsorzioConducente"
+	currentTemplate = getattr(doc, 'lastTemplateID', doc.pageTemplates[0].id)
+	if currentTemplate <> templateID:
+		#print "Cambio da %s a %s" % (getattr(doc, 'lastTemplateID', None), templateID)
+		canvas.apply_page_numbers()
+		doc.lastTemplateID = templateID
+
+	descrittore_fattura = doc.fattura.descrittore
+	doc.fattura.descrittore = lambda:"consorzio"
+	onPage(canvas, doc, da=settings.DATI_CONSORZIO, a=doc.fattura.emessa_a)
+	doc.fattura.descrittore = descrittore_fattura
+
+
+def render_to_reportlab(context):
+	fattura = context.get('fattura')
+	fattura.footer = context['invoices_footer']
+
+	response = http.HttpResponse(mimetype='application/pdf')
+
+	frameT = Frame(1,1,100,100, id='normal')
+#	pageTemplates = [TemplateFattura(frames = [frameT])]
+
+	if fattura.tipo == "3":
+		pageTemplates = [PageTemplate(id='ConducenteConsorzio', onPage=onPageConducenteConsorzio),
+						 PageTemplate(id='ConsorzioConducente', onPage=onPageConsorzioConducente)
+						]
+	else:
+		pageTemplates = [PageTemplate(id='Normale', onPage=onPageNormal, frames=frameT)]
+
+	width, height = portrait(A4)
+
+	doc = BaseDocTemplate(response,
+							pagesize=(width, height),
+							leftMargin=1 * cm,
+							rightMargin=1 * cm,
+							bottomMargin=1.5 * cm,
+							topMargin=1 * cm,
+							showBoundary=test,
+							pageTemplates=pageTemplates,
+						)
+	doc.fattura = fattura
 
 	righeFattura = [
 					('Descrizione', 'Q.tà', 'Prezzo', 'IVA %', 'Importo'),
@@ -307,8 +252,13 @@ def render_to_reportlab(context):
 	story = [ Table(righeFattura, style=righeStyle, repeatRows=1, colWidths=colWidths) ]
 	story.append(KeepTogether(Table(righeTotali, style=totaliStyle,
 									colWidths=(width - doc.leftMargin - doc.rightMargin - 1.6 * cm, 1.6 * cm))))
-	#story.append(Spacer(0, 0.5 * cm))
-	#story.append(note_finali)
 
+	if fattura.tipo == "3":	# le ricevute si raddoppiano con 2 template diversi
+		story = story + [NextPageTemplate("ConsorzioConducente"), PageBreak()] + story
 	doc.build(story, canvasmaker=NumberedCanvas)
 	return response
+
+if __name__ == '__main__':
+	from fatturazione.models import Fattura
+	fattura = Fattura.objects.get(id=211)
+	render_to_reportlab(context={"fattura":fattura, 'invoices_footer':settings.INVOICES_FOOTERS})
