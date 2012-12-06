d4b0952fef749b7798058d1e03b4d90c5fb24326
unknown <dariosky@.MION.COM>
2012-12-06 14:40:45 +0100

    Unificata la modifica con la creazione. Corretta la modifica.

--- prenotazioni/urls.py
+++ prenotazioni/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import * #@UnusedWildImport
 
 urlpatterns = patterns('prenotazioni.views.main',
     url(r'^$', 'prenota', name="tamPrenotazioni"),
+    url(r'^edit/(?P<id_prenotazione>\d+)/$', 'prenota', name="tamPrenotazioni-edit"),
+    
     url(r'^storico/$', 'cronologia', name="tamCronoPrenotazioni"),
-    url(r'^edit/(?P<id_prenotazione>\d+)/$', 'edit', name="tamPrenotazioni-edit"),
 )
