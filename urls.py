6266907f094759d1c6a915c72cc704aa290e131b
Dario Varotto <dariosky@gmail.com>
2013-03-29 11:49:56 +0100

    Aggiungo PLUGGABLE_APPS ai settings per una lista delle applicazioni opzionali incluse. Mi comporto di conseguenza.

--- urls.py
+++ urls.py
@@ -6,32 +6,46 @@ import os
 #	url(r'^$', "django.views.generic.simple.direct_to_template", {'template':'main.html'}, name="main" ),
 #)
 
-urlpatterns = patterns ('',
-	(r'', include('tam.urls')),
-	(r'', include('modellog.urls')),
-	(r'^archive/', include('tamArchive.urls')),
-	#( r'', include( 'license.urls' ) ),
-	(r'^fatture/', include('fatturazione.urls')),
-	(r'^prenotazioni/', include('prenotazioni.urls')),
- )
+urlpatterns = patterns('',
+                       (r'', include('tam.urls')),
+                       (r'', include('modellog.urls')),
+                       (r'^archive/', include('tamArchive.urls')),
+                       #( r'', include( 'license.urls' ) ),
+                       (r'^fatture/', include('fatturazione.urls')),
+)
+
+# add pluggable apps URL
+for app, desc in settings.PLUGGABLE_APPS.items():
+	if 'urls' in desc:
+		url_regex, import_location = desc['urls']
+		urlpatterns += patterns('',
+		                        url(url_regex, include(import_location)),
+		)
 
 from django.contrib import admin
+
 admin.autodiscover()
 urlpatterns += patterns('',
-	url(r'^admin/', include(admin.site.urls)),
+                        url(r'^admin/', include(admin.site.urls)),
 )
 # Serve media settings to simulate production, we know in REAL production this won't happend
 if settings.DEBUG:
 	urlpatterns += patterns('',
-		#mediaprod > _generated_media 
-		("^" + settings.PRODUCTION_MEDIA_URL[1:] + r'(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), '_generated_media')}),
-		#media > /media
-		("^media/" + r'(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), "media")}),
+	                        #mediaprod > _generated_media
+	                        ("^" + settings.PRODUCTION_MEDIA_URL[1:] + r'(?P<path>.*)$',
+	                         'django.views.static.serve',
+	                         {'document_root': os.path.join(os.path.dirname(__file__), '_generated_media')}),
+
+	                        #media > /media
+	                        ("^media/" + r'(?P<path>.*)$', 'django.views.static.serve',
+	                         {'document_root': os.path.join(os.path.dirname(__file__), "media")}),
 	)
+
 
 def errorview(request):
 	raise Exception("Eccezione di test.")
 
+
 urlpatterns += patterns('',
-					url ('^500/', errorview, name='error-test')
-				)
+                        url('^500/', errorview, name='error-test')
+)
