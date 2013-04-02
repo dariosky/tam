34076a893f9fcb1f8342ef0e38c77b3034b3a391
Dario Varotto <dariosky@gmail.com>
2013-04-02 15:01:50 +0200

    XLS back to 3000

--- tam/views/tamXml.py
+++ tam/views/tamXml.py
@@ -1,5 +1,7 @@
 #coding:utf8
+from modellog.actions import logAction
 import xlsUtil
+import xlwt
 from django.http import HttpResponse, HttpResponseRedirect
 import logging
 import StringIO
@@ -24,6 +26,7 @@ corseField['cartaDiCredito'] = 'Carta?'
 corseField['conducente.nick'] = 'Conducente'
 corseField['note'] = 'note'
 
+
 def djangoManagerToTable(self, fields):
 	""" Should return a list of tables
 		Take a list of django object managers and a list of fields to return as input
@@ -38,28 +41,35 @@ def djangoManagerToTable(self, fields):
 		for field in fields.keys():
 			if "." in field:
 				related = record
-				for token in field.split('.'):	# iterate sub object with .
+				for token in field.split('.'):    # iterate sub object with .
 					if related:
 						related = related.__getattribute__(token)
 					else:
 						break
 			else:
+
 				related = record.__getattribute__(field)
 
-			if callable(related): related = related()	# if it's a method call it
+			if callable(related):
+				related = related()    # if it's a method call it
 			row.append(related)
 		table.append(row)
-	return (table,)	# only one result sheet with our table
+	return (table,)    # only one result sheet with our table
+
 
 def xlsResponse(request, querySet):
-	doc = xlsUtil.Workbook()
+	doc = xlwt.Workbook()
 
 	numViaggi = querySet.count()
 	logging.debug("Export to EXCEL %d viaggi." % numViaggi)
-	maxViaggi = 1500
+	maxViaggi = 3000
 	if numViaggi > maxViaggi:
 		messages.error(request, "Non puoi esportare in Excel più di %d viaggi contemporaneamente." % maxViaggi)
-		return HttpResponseRedirect("/")	# back home
+		return HttpResponseRedirect("/")    # back home
+
+	# from guppy import hpy
+	# h = hpy()
+	# h.setref()
 
 	querySet = querySet.select_related("da", "a", "cliente", "conducente", "passeggero")
 	generatore = xlsUtil.Sql2xls(doc, manager=querySet)
@@ -70,19 +80,21 @@ def xlsResponse(request, querySet):
 	if generatore.sheetCount:
 		output = StringIO.StringIO()
 
-#			filename=os.path.join( os.path.dirname(__file__), 'tempExcel.xls')
-#			doc.save(filename)
-#			responseFile=file(filename,"rb")
-#			response = HttpResponse(responseFile.read(), mimetype='application/excel')
-#			responseFile.close()
+		#			filename=os.path.join( os.path.dirname(__file__), 'tempExcel.xls')
+		#			doc.save(filename)
+		#			responseFile=file(filename,"rb")
+		#			response = HttpResponse(responseFile.read(), mimetype='application/excel')
+		#			responseFile.close()
 
-		doc.save(output)	# save the excel
+		doc.save(output)    # save the excel
 		output.seek(0)
 		response = HttpResponse(output.getvalue(), mimetype='application/excel')
 
 		response['Content-Disposition'] = 'attachment; filename="%s"' % 'tamExport.xls'
+		logAction(action='X', description="Export in Excel di %d corse." % numViaggi, user=request.user, log_date=None)
+		# print h.heap()
 		return response
 	else:
 		messages.error(request, "Non ho alcun viaggio da mettere in Excel, cambia i filtri per selezionarne qualcuno.")
 		del generatore
-		return HttpResponseRedirect("/")	# back home
+		return HttpResponseRedirect("/")    # back home
