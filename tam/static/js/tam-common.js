197f6d0a3d7ecc5f150f1cf4ad57100c9980f103
Dario Varotto <dariosky@gmail.com>
2013-08-06 22:58:50 +0200

    rendo scrollableGoto globale perché django-pipeline mi impedisce che le funzioni definiscano dei global

--- tam/static/js/tam-common.js
+++ tam/static/js/tam-common.js
@@ -1,16 +1,18 @@
 /* funzioni JS comuni ovunque in tam */
-if( typeof $ != "undefined") {
-	$(document).ready(function() {
-		$('.messages').live("click", function() {
-			$(this).fadeOut('slow', function(){$(this).remove()});
+if (typeof $ != "undefined") {
+	$(document).ready(function () {
+		$('.messages').live("click", function () {
+			$(this).fadeOut('slow', function () {
+				$(this).remove()
+			});
 			return false;
-		})
+		});
 
-		$(document).bind('keydown', 'esc', function() {
-			if (!confirm("Sicuro di volerti disconnettere?")) return;
+		$(document).bind('keydown', 'esc', function () {
+			if (!confirm("Sicuro di volerti disconnettere?")) return false;
 			var overlay = $("<div style='position:fixed; left:50%; top:50%; width:300px; height:200px; margin-top:-200px; margin-left:-150px; font-size:2em; font-family:Times; text-align:center; z-index:1000;'>" + "Disconnessione in corso" + "</div>");
 			$('.content').animate({
-				opacity : 0.25
+				opacity: 0.25
 			});
 			$('body').append(overlay);
 			location.href = '/logout/';
@@ -18,7 +20,7 @@ if( typeof $ != "undefined") {
 		});
 
 		if (typeof moveToNext != "undefined") {
-			$(document).bind('keydown', 'n', function() {
+			$(document).bind('keydown', 'n', function () {
 				moveToNext();
 				return false;
 			});
@@ -27,32 +29,32 @@ if( typeof $ != "undefined") {
 }
 
 function messageBox(msg, classes) {
-  var msgBox = $('.messages');
-  var message = $('<div class="alert"/>').html(msg);
-  if (msgBox.length === 0) {
-      msgBox = $('<div />', { "id":'msg-box', "class":'messages '+classes}).hide().prependTo('body');
-      msgBox.append(message).fadeIn('slow');
-  }
-  else {
-  	msgBox.append(message);
-  }
+	var msgBox = $('.messages');
+	var message = $('<div class="alert"/>').html(msg);
+	if (msgBox.length === 0) {
+		msgBox = $('<div />', { "id": 'msg-box', "class": 'messages ' + classes}).hide().prependTo('body');
+		msgBox.append(message).fadeIn('slow');
+	}
+	else {
+		msgBox.append(message);
+	}
 }
 
-function scrollableGoto(scroller) {
+window.scrollableGoto = function scrollableGoto(scroller) {
 	var scrollParent = scroller.parent();
 	//var deltaH = scroller.offset().top - scrollParent.scrollTop();
 	//console.log("deltaH:"+deltaH);
 	deltaH = 0;
 	//console.log("Scroll da "+scroller_min+" a "+scroller_max);
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 		if (!scroller.is(":visible")) return;
 		var scroller_min = scrollParent.offset().top;
-		var scroller_max = scrollParent.offset().top+scrollParent.height();
+		var scroller_max = scrollParent.offset().top + scrollParent.height();
 		//$(window).height()/2
-		var newTop = $(window).scrollTop()+deltaH;
-		if (newTop>scroller_max) newTop=scroller_max;
-		if (newTop<scroller_min) newTop=scroller_min;
+		var newTop = $(window).scrollTop() + deltaH;
+		if (newTop > scroller_max) newTop = scroller_max;
+		if (newTop < scroller_min) newTop = scroller_min;
 		//console.log("set to "+newTop+" ["+scroller_min+","+scroller_max+"]");
-		scroller.stop().animate({"top": newTop+"px"}, "fast");
+		scroller.stop().animate({"top": newTop + "px"}, "fast");
 	});
 }
