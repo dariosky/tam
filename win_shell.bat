121f8c7747d713f1374675c02f7dc56051e294c4
Dario Varotto <dariosky@gmail.com>
2014-02-09 23:40:56 +0100

    Fix on shell

--- win_shell.bat
+++ win_shell.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-cmd /e:on /k "call ..\.environments\tam\Scripts\activate.bat && set DJANGO_SETTINGS_MODULE=settings"
+cmd /e:on /k "call C:\Stuff\development\Python\.environments\tam\Scripts\activate.bat && set DJANGO_SETTINGS_MODULE=settings"
