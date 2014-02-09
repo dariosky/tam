29416f3e232954aaa5f0ce252f9236c8d34ca473
Dario Varotto <dariosky@gmail.com>
2014-02-09 23:37:36 +0100

    Merged TAM development branches. From now on I'll use only one branch with different configurable settings (by os.environ)

---
+++ win_arte_settings.bat
@@ -0,0 +1,2 @@
+set tamport=8005
+echo Development server will run on localhost:%tamport%
--- win_shell.bat
+++ win_shell.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-cmd /e:on /k "call ..\environment\Scripts\activate.bat && set DJANGO_SETTINGS_MODULE=settings"
+cmd /e:on /k "call ..\.environments\tam\Scripts\activate.bat && set DJANGO_SETTINGS_MODULE=settings"
