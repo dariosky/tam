5c9967fe5c4c9360dd1fe734b5d55a0eeffc5e42
Dario Varotto <dariosky@gmail.com>
2014-02-16 17:34:30 +0100

    Deploy run_server script for start/stop server checking it's running for crontab usage

--- fabfile.py
+++ fabfile.py
@@ -248,9 +248,72 @@ def send_secrets(secret_files=None, ask=False):
 			run('ln -s -f %s settings_local.py' % secret_files[0])
 
 
-def run_command_content():
-	gunicorn_command = get_gunicorn_command(daemon=False)
-	return '#!/bin/bash\n' + 'exec %s' % gunicorn_command
+def run_command_content(daemon=True):
+	default_args = "--workers={GUNICORN_WORKERS} --log-level=warning -t {GUNICORN_WORKERS_TIMEOUT}"
+	if daemon:
+		default_args += " --daemon"
+	env.GUNICORN_ARGUMENTS = default_args.format(**env)
+	env.WSGI_APPLICATION = "wsgi:application"
+
+	# prepare the variables
+	content = """#!/bin/bash
+GUNICORN_CMD={VENV_FOLDER}/bin/gunicorn
+GUNICORN_ARGS="{GUNICORN_ARGUMENTS}"
+LOG_PATH={GUNICORN_LOGFILE}
+PID_PATH={GUNICORN_PID_FILE}
+PORT={GUNICORN_PORT}
+
+start_server () {{
+  if [ -f $PID_PATH ]; then
+    if [ "$(ps -p `cat $PID_PATH` | wc -l)" -gt 1 ]; then
+       echo "A server is already running on port $PORT"
+       return
+    else
+    	echo "We have the PID but not the process deleting PID and restarting"
+    	rm $PID_PATH
+    fi
+  fi
+  #cd $PROJECTLOC
+  echo "starting gunicorn on port $PORT"
+  $GUNICORN_CMD $GUNICORN_ARGS -b 127.0.0.1:$PORT --log-file $LOG_PATH --pid $PID_PATH {WSGI_APPLICATION}
+}}
+
+stop_server () {{
+  if [ -f $PID_PATH ] && [ "$(ps -p `cat $PID_PATH` | wc -l)" -gt 1 ]; then
+    echo "stopping server on port $PORT"
+    kill -9  `cat $PID_PATH`
+    rm $PID_PATH
+  else
+    if [ -f $PID_PATH ]; then
+      echo "server not running"
+      rm $PID_PATH
+    else
+      echo "No pid file found for server"
+    fi
+  fi
+}}
+
+case "$1" in
+'start')
+  start_server
+  ;;
+'stop')
+  stop_server
+  ;;
+'restart')
+  stop_server
+  sleep 2
+  start_server
+  ;;
+*)
+  echo "Usage: $0 {{ start | stop | restart }}"
+  ;;
+esac
+
+exit 0
+#exec $GUNICORN_CMD $GUNICORN_ARGS -b 127.0.0.1:$PORT --log-file $LOG_PATH --pid $PID_PATH {WSGI_APPLICATION}
+	"""
+	return content.format(**env)
 
 
 @task
@@ -268,19 +331,18 @@ def create_run_command():
 @task
 def local_create_run_command():
 	puts("Creating run_server command to be run with supervisor.")
-	with lcd(env.REPOSITORY_FOLDER):
-		with file(posixpath.join(env.REPOSITORY_FOLDER, 'run_server'), 'w') as runner:
+	with lcd(env.localfolder):
+		with file('run_server', 'w') as runner:
 			runner.write(run_command_content())
 
 
 @task
 def local_create_run_command():
 	gunicorn_command = get_gunicorn_command(daemon=False)
-	puts("Creating run_server command to be run with supervisor.")
-	with lcd(env.REPOSITORY_FOLDER):
-		with file(posixpath.join(env.REPOSITORY_FOLDER, 'run_server'), 'w') as runner:
-			runner.write('#!/bin/bash\n')
-			runner.write('exec %s' % gunicorn_command)
+	puts("Creating run_server command to be run " + ("with" if env.USE_SUPERVISOR else "without") + " supervisor.")
+	with lcd(env.localfolder):
+		with file('run_server', 'w') as runner:
+			runner.write(run_command_content())
 
 
 @task
