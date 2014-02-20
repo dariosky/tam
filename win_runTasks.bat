279c2cd67fd11538fe8812d1d2cf8476916e04cf
Dario Varotto <dariosky@gmail.com>
2014-02-20 21:31:22 +0100

    Taxi2: sconto fisso 2% per commissioni carta di credito

--- win_runTasks.bat
+++ win_runTasks.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d %~dp0
-call ..\.environments\tam\scripts\activate.bat
+call C:\Stuff\development\Python\.environments\tam\scripts\activate.bat
+set TAM_SETTINGS=settings_arte
 set DJANGO_SETTINGS_MODULE=settings
 title TASKD
 start python manage.py taskd run
