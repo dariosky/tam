118c993b6d695ccfc5c1732af8f3cc461dd244fa
Dario Varotto <dariosky@gmail.com>
2014-04-24 10:51:31 +0200

    Visualizzazione delle presenze per conducente/tipo

--- calendariopresenze/urls.py
+++ calendariopresenze/urls.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 from django.conf.urls import url, patterns
-from calendariopresenze.views import CalendarManage, CalendarRank
+from calendariopresenze.views import CalendarManage, CalendarRank, CalendarByConducente
 
 urlpatterns = patterns('calendariopresenze.views',
                        url(r'set/', CalendarManage.as_view(), name='calendariopresenze-manage'),
                        url(r'rank/(?P<year>\d{4})/', CalendarRank.as_view(), name='calendariopresenze-rank'),
                        url(r'rank/', CalendarRank.as_view(), name='calendariopresenze-rank'),
+                       url(r'view/(?P<year>\d{4})/(?P<conducente_id>\d+)/(?P<caltype>\d+)/',
+                           CalendarByConducente.as_view(), name='calendariopresenze-view')
 
 )
