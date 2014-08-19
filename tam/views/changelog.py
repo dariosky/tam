f7afc5beec313f438c816e92005c678de6651753
Dario Varotto <dariosky@gmail.com>
2014-08-19 17:53:07 +0200

    Cambiamenti all'interfaccia per essere più iPad friendly

--- tam/views/changelog.py
+++ tam/views/changelog.py
@@ -143,6 +143,7 @@ def changeLog(request, template_name='static/changelog.html'):
 		('6.1', '11/7/2014', "Conservazione sicura degli allegati alle prenotazioni.") if 'prenotazioni' in settings.PLUGGABLE_APPS else "",
 		('6.11', '22/7/2014', "Fix board per device touch.") if 'board' in settings.PLUGGABLE_APPS else "",
 		('6.12', '15/8/2014', "Archiviazione con data personalizzabile e aggiornamenti vari."),
+		('6.15', '19/8/2014', "Cambiamenti al layout, mobile friendly."),
 	]
 	# ('', '', mark_safe('''''')),
 	for version in known_changes[::-1]:
