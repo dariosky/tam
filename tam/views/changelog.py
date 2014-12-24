1e5dcfbee586bde4456f0c96d8ac7e69f8ab24cf
Dario Varotto <dariosky@gmail.com>
2014-12-24 16:25:14 +0100

    Pingview

--- tam/views/changelog.py
+++ tam/views/changelog.py
@@ -146,6 +146,7 @@ def changeLog(request, template_name='static/changelog.html'):
 		('6.15', '19/8/2014', "Cambiamenti al layout, mobile friendly."),
 		('6.16', '6/10/2014', "Nascondo i clienti inattivi."),
 		('6.17', '2/11/2014', "CSRF e clickjacking protections."),
+		('6.18', '2/11/2014', "Django 1.7 e varie migliorie."),
 	]
 	# ('', '', mark_safe('''''')),
 	for version in known_changes[::-1]:
