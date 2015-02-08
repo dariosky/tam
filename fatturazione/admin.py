569c1ea4a4d1996f12d672d4419fd001ef9bafda
Dario Varotto <dariosky@gmail.com>
2015-02-08 23:00:24 +0100

    Some bugfixes

--- fatturazione/admin.py
+++ fatturazione/admin.py
@@ -1,5 +1,11 @@
+# coding=utf-8
 from fatturazione.models import Fattura, RigaFattura
 from django.contrib import admin
 
-admin.site.register(Fattura)
+
+class FatturaAdmin(admin.ModelAdmin):
+	date_hierarchy = 'data'
+
+
+admin.site.register(Fattura, FatturaAdmin)
 admin.site.register(RigaFattura)
