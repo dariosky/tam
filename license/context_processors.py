de7ffd202ded70212c97809a531e56e7246f921b
Dario Varotto <dariosky@gmail.com>
2015-07-28 20:37:13 +0200

    Fix: missing request on the context_processors

--- license/context_processors.py
+++ license/context_processors.py
@@ -1,19 +1,22 @@
+# coding=utf-8
 from django.utils.http import urlquote
-__author__="Dario Varotto"
-__date__ ="$30-dic-2010 17.04.59$"
+
+__author__ = "Dario Varotto"
+__date__ = "$30-dic-2010 17.04.59$"
 
 from license_functions import get_license_detail
 from django.conf import settings
 
+
 def license_details(request):
-	""" Add license detail to request context """
-	result=get_license_detail()
-	result["tam_version"] = settings.TAM_VERSION
-	result["tam_stealth"] = settings.DEBUG
-	
-	# Aggiungo al contesto il percorso completo della richiesta (mi serve per i "?next=") 
-	if request.META["QUERY_STRING"]:
-		result["full_request_path"] = request.path + "?" + urlquote(request.META["QUERY_STRING"])
-	else:
-		result["full_request_path"] = request.path
-	return result
+    """ Add license detail to request context """
+    result = get_license_detail()
+    result["tam_version"] = settings.TAM_VERSION
+    result["tam_stealth"] = settings.DEBUG
+
+    # Aggiungo al contesto il percorso completo della richiesta (mi serve per i "?next=")
+    if request.META["QUERY_STRING"]:
+        result["full_request_path"] = request.path + "?" + urlquote(request.META["QUERY_STRING"])
+    else:
+        result["full_request_path"] = request.path
+    return result
--- tam/middleware/loginRequirement.py
+++ tam/middleware/loginRequirement.py
@@ -14,57 +14,57 @@ logger = logging.getLogger('tam.general')
 
 
 class RequireLoginMiddleware(object):
-	"""Middleware that gets various objects from the
-	request object and saves them in thread local storage."""
+    """Middleware that gets various objects from the
+    request object and saves them in thread local storage."""
 
-	def process_view(self, request, view_func, view_args, view_kwargs):
-		public_view = getattr(view_func, 'public', False)
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        public_view = getattr(view_func, 'public', False)
 
-		# public view or named "serve" <= (django.views.static.serve)
-		if public_view or view_func.func_name == "serve":
-			return None
-		else:
-			# not a public view, if not authenticaded ask for login
-			if not request.user.is_authenticated():
-				return login_required(view_func)(request, *view_args, **view_kwargs)
+        # public view or named "serve" <= (django.views.static.serve)
+        if public_view or view_func.func_name == "serve":
+            return None
+        else:
+            # not a public view, if not authenticaded ask for login
+            if not request.user.is_authenticated():
+                return login_required(view_func)(request, *view_args, **view_kwargs)
 
-			prenotazioni_view = getattr(view_func, 'prenotazioni', False)
-			try:
-				prenotazioni_user = request.user.prenotazioni
-			except ObjectDoesNotExist:
-				prenotazioni_user = False
+            prenotazioni_view = getattr(view_func, 'prenotazioni', False)
+            try:
+                prenotazioni_user = request.user.prenotazioni
+            except ObjectDoesNotExist:
+                prenotazioni_user = False
 
-			if prenotazioni_user and not prenotazioni_view:
-				# messages.error(request, "Gli utenti del servizio prenotazioni non hanno accesso a questa pagina.")
-				return HttpResponseRedirect(reverse('tamPrenotazioni'))
+            if prenotazioni_user and not prenotazioni_view:
+                # messages.error(request, "Gli utenti del servizio prenotazioni non hanno accesso a questa pagina.")
+                return HttpResponseRedirect(reverse('tamPrenotazioni'))
 
-			elif not prenotazioni_user and prenotazioni_view:
-				# messages.error(request, "I conducenti non possono accedere alle prenotazioni.")
-				return HttpResponseRedirect(reverse('tamCorse'))
-			else:
-				if request.user.is_authenticated():
-					# everything is OK proceed with other middlewares
-					return None
-				else:
-					# we are not logged in so let login_required redirect us
-					return login_required(view_func)(request, *view_args, **view_kwargs)
+            elif not prenotazioni_user and prenotazioni_view:
+                # messages.error(request, "I conducenti non possono accedere alle prenotazioni.")
+                return HttpResponseRedirect(reverse('tamCorse'))
+            else:
+                if request.user.is_authenticated():
+                    # everything is OK proceed with other middlewares
+                    return None
+                else:
+                    # we are not logged in so let login_required redirect us
+                    return login_required(view_func)(request, *view_args, **view_kwargs)
 
 
 def csrf_failure_view(request, reason=''):
-	response = render_to_response('403.html', {}, context_instance=RequestContext(request))
-	response.status_code = 403
-	message_tokens = [u"%s got hit in the face with Forbidden shovel" % request.user]
-	message_tokens.append("in: %s" % request.path)
-	post_data = request.POST.copy()
-	if 'password' in post_data:
-		del post_data['password']
-	if 'HTTP_REFERER' in request.environ:
-		message_tokens.append("from: %s" % request.environ['HTTP_REFERER'])
-	message_tokens.append("POST:\n" + unicode(post_data))
-	if False:    # ok, let's disable notifications of 403, if not settings.DEBUG
-		mail_admins(
-			"403 alert",
-			"\n".join(message_tokens)
-		)
-	logger.debug("\n".join(message_tokens))
-	return response
+    response = render_to_response('403.html', {}, context_instance=RequestContext(request))
+    response.status_code = 403
+    message_tokens = [u"%s got hit in the face with Forbidden shovel" % request.user]
+    message_tokens.append("in: %s" % request.path)
+    post_data = request.POST.copy()
+    if 'password' in post_data:
+        del post_data['password']
+    if 'HTTP_REFERER' in request.environ:
+        message_tokens.append("from: %s" % request.environ['HTTP_REFERER'])
+    message_tokens.append("POST:\n" + unicode(post_data))
+    if False:  # ok, let's disable notifications of 403, if not settings.DEBUG
+        mail_admins(
+            "403 alert",
+            "\n".join(message_tokens)
+        )
+    logger.debug("\n".join(message_tokens))
+    return response
