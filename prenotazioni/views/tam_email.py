03f5065ab8069fd4cceb79e713be82251f5ac230
Dario Varotto <dariosky@gmail.com>
2016-02-28 22:24:00 +0100

    Use mailgun as email backend. View for email testing

--- prenotazioni/views/tam_email.py
+++ prenotazioni/views/tam_email.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 from django.conf import settings
 from django.template import Context
 from django.template.loader import get_template
@@ -9,99 +9,106 @@ import logging
 from prenotazioni.models import Prenotazione
 from django.utils.safestring import mark_safe
 
-ADMIN_MAIL = ",".join(["%s <%s>" % (name, email) for (name, email) in settings.ADMINS])
+ADMIN_MAIL = ",".join(["%s <%s>" % adm for adm in settings.ADMINS])
+logger = logging.getLogger('tam.mail')
 
 
 def notifyByMail(to=None, subject="",
-                 context=None, contextText=None, messageTxtTemplateName=None,
-                 messageHtmlTemplateName=None, bcc=None,
+                 context=None, contextText=None,
+                 messageTxtTemplateName=None, messageHtmlTemplateName=None, bcc=None,
+                 body=None,
                  reply_to=None, attachments=None,
                  from_email=None,
                  **kwargs):
-	""" Warn with a mail when some event occour """
-	if not context: context = {}
-	if not attachments: attachments = []
-	from_email = from_email or settings.DEFAULT_FROM_EMAIL
+    """ Warn with a mail when some event occour """
+    if not context:
+        context = {}
+    if not attachments:
+        attachments = []
+    from_email = from_email or settings.DEFAULT_FROM_EMAIL
 
-	signatureTXT = settings.DATI_CONSORZIO
+    signatureTXT = settings.DATI_CONSORZIO
 
-	signatureHTML = getattr(settings, "DATI_CONSORZIO_HTML", None)
-	if signatureHTML is None:
-		signatureHTML = settings.DATI_CONSORZIO
-	else:
-		signatureHTML = mark_safe(signatureHTML)
+    signatureHTML = getattr(settings, "DATI_CONSORZIO_HTML", None)
+    if signatureHTML is None:
+        signatureHTML = settings.DATI_CONSORZIO
+    else:
+        signatureHTML = mark_safe(signatureHTML)
 
-	context["signatureHTML"] = signatureHTML
+    context["signatureHTML"] = signatureHTML
 
-	if to is None: to = [ADMIN_MAIL]
-	if settings.DEBUG:
-		to = [ADMIN_MAIL]
-		bcc = []
-		subject = "[TEST] " + subject
-	context_html = Context(context)
-	if contextText is None:
-		context_text = context_html
-	else:
-		context_text = Context(contextText)
+    if to is None:
+        to = [ADMIN_MAIL]
+    if settings.DEBUG:
+        logger.debug("In test, send mail only to ADMIN instead of %s" % to)
+        to = [ADMIN_MAIL]
+        bcc = []
+        subject = "[TEST] " + subject
+    context_html = Context(context)
+    if contextText is None:
+        context_text = context_html
+    else:
+        context_text = Context(contextText)
+    if body and (messageTxtTemplateName or messageHtmlTemplateName):
+        raise Exception("Give the Body OR the template")
+    if body:
+        messageText = body
+        htmlMessage = None
+    else:
+        messageTemplate = get_template(messageTxtTemplateName)
+        messageText = messageTemplate.render(context_text)
+        if messageHtmlTemplateName:
+            htmlTemplate = get_template(messageHtmlTemplateName)
+            htmlMessage = htmlTemplate.render(context_html)
+        else:  # default html message
+            htmlTemplate = get_template("prenotazioni_email/htmlMailTemplate.html")
+            htmlMessage = htmlTemplate.render(Context({"message": messageText}))  # generic html message
 
-	messageTemplate = get_template(messageTxtTemplateName)
-	messageText = messageTemplate.render(context_text)
-	if messageHtmlTemplateName:
-		htmlTemplate = get_template(messageHtmlTemplateName)
-		htmlMessage = htmlTemplate.render(context_html)
-	else:  # default html message
-		htmlTemplate = get_template("prenotazioni_email/htmlMailTemplate.html")
-		htmlMessage = htmlTemplate.render(Context({"message": messageText}))  # generic html message
+    if signatureTXT:
+        messageText += u"\n-- \n" + signatureTXT
 
-	if signatureTXT:
-		messageText += u"\n-- \n" + signatureTXT
+    emailMessage = EmailMultiRelated(
+        body=messageText,
+        subject=subject,
+        to=to,
+        bcc=bcc,
+        from_email=from_email,
+    )
+    if reply_to:
+        emailMessage.extra_headers['Reply-To'] = reply_to
 
-	emailMessage = EmailMultiRelated(
-		body=messageText,
-		subject=subject,
-		to=to,
-		bcc=bcc,
-		from_email=from_email,
-	)
-	if reply_to:
-		emailMessage.extra_headers['Reply-To'] = reply_to
+    if htmlMessage:
+        emailMessage.attach_alternative(htmlMessage, "text/html")
 
-	emailMessage.attach_alternative(htmlMessage, "text/html")
+    f = open(os.path.join(settings.MEDIA_ROOT, settings.EMAIL_SMALL_LOGO), 'rb')
+    logo_content = f.read()
+    f.close()
+    emailMessage.attach_related("logo", logo_content, 'image/jpeg')
 
-	f = open(os.path.join(settings.MEDIA_ROOT, settings.EMAIL_SMALL_LOGO), 'rb')
-	logo_content = f.read()
-	f.close()
-	emailMessage.attach_related("logo", logo_content, 'image/jpeg')
+    if attachments:
+        # We have a multipart/related message with inline attachments
+        # create a container of multipart/mixed and include the related and the attached files
+        for attachment in attachments:
+            if isinstance(attachment, MIMEBase):
+                emailMessage.attach(attachment)
+            else:
+                emailMessage.attach_file(attachment)
+                # emailMessage.attach('event.ics', 'sgnafuz', 'text/calendar')
 
-	if attachments:
-		# We have a multipart/related message with inline attachments
-		# create a container of multipart/mixed and include the related and the attached files
-		for attachment in attachments:
-			if isinstance(attachment, MIMEBase):
-				emailMessage.attach(attachment)
-			else:
-				emailMessage.attach_file(attachment)
-			#emailMessage.attach('event.ics', 'sgnafuz', 'text/calendar')
-
-	if settings.DEBUG:
-		print("In test, don't send mail to %s" % to)
-		emailMessage.send()
-	#print htmlMessage
-	else:
-		logging.debug("Sending mail to %s" % to)
-		emailMessage.send()
+    logging.debug("Sending mail to %s" % to)
+    emailMessage.send()
 
 
 if __name__ == "__main__":
-	prenotazione = Prenotazione.objects.get(id=1)
-	notifyByMail(
-		to=[prenotazione.owner.email, settings.EMAIL_CONSORZIO],
-		subject="Conferma prenotazione TaM n° %d" % prenotazione.id,
-		context={
-		"prenotazione": prenotazione,
-		},
-		reply_to=settings.EMAIL_CONSORZIO,
-		messageTxtTemplateName="prenotazioni_email/conferma.inc.txt",
-		messageHtmlTemplateName="prenotazioni_email/conferma.inc.html",
-	)
-	print "Fine."
+    prenotazione = Prenotazione.objects.get(id=1)
+    notifyByMail(
+        to=[prenotazione.owner.email, settings.EMAIL_CONSORZIO],
+        subject="Conferma prenotazione TaM n° %d" % prenotazione.id,
+        context={
+            "prenotazione": prenotazione,
+        },
+        reply_to=settings.EMAIL_CONSORZIO,
+        messageTxtTemplateName="prenotazioni_email/conferma.inc.txt",
+        messageHtmlTemplateName="prenotazioni_email/conferma.inc.html",
+    )
+    print "Fine."
--- tam/views/manviews.py
+++ tam/views/manviews.py
@@ -1,28 +1,46 @@
 # coding=utf-8
 import django
+from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render_to_response
 import sys
 from django.http import HttpResponse
 from django.template import Template, RequestContext
 
+from prenotazioni.views.tam_email import ADMIN_MAIL, notifyByMail
+
 
 def errors500(request, template_name='500.html'):
-	exc_type, exc, trackback = sys.exc_info()
-	return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+    exc_type, exc, trackback = sys.exc_info()
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 
 def errors400(request, template_name='404.html'):
-	return render_to_response(template_name, {}, context_instance=RequestContext(request))
+    return render_to_response(template_name, {}, context_instance=RequestContext(request))
 
 
 def errorview(request):
-	# mail_admins(subject='Message admins', message='Bla bla bla')
-	raise Exception("Eccezione di test.")
+    # mail_admins(subject='Message admins', message='Bla bla bla')
+    raise Exception("Eccezione di test.")
 
 
 def pingview(request):
-	error = False
-	results = ['{{host}}', "ok from django %s" % ".".join(map(str, django.VERSION[:3]))]
+    error = False
+    results = ['{{host}}', "ok from django %s" % ".".join(map(str, django.VERSION[:3]))]
 
-	response = Template('\n'.join(results)).render(RequestContext(request))
-	return HttpResponse(response, status=200 if not error else 500, content_type='text/plain')
+    response = Template('\n'.join(results)).render(RequestContext(request))
+    return HttpResponse(response, status=200 if not error else 500, content_type='text/plain')
+
+
+@user_passes_test(lambda user: user.is_superuser)
+def email_test(request):
+    results = ["Email sent to admins: %s" % ADMIN_MAIL]
+    results.append("backend: %s" % settings.EMAIL_BACKEND)
+    notifyByMail(
+        to=[ADMIN_MAIL],
+        subject="Test email with %s" % settings.EMAIL_BACKEND,
+        body="This is the body of the message",
+
+    )
+    response = Template('\n'.join(results)).render(RequestContext(request))
+    return HttpResponse(response, status=200, content_type='text/plain')
