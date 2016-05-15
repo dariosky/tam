a89650fdc8b678322e26e058dbc4697091406cd2
Dario Varotto <dariosky@gmail.com>
2016-05-15 22:26:56 +0200

    Multi-device check don't apply to hotel user

--- codapresenze/models.py
+++ codapresenze/models.py
@@ -4,19 +4,20 @@ from django.contrib.auth.models import User
 
 
 class CodaPresenze(models.Model):
-	data_accodamento = models.DateTimeField(auto_now_add=True, editable=True)
-	utente = models.ForeignKey(User)
-	luogo = models.TextField(max_length=30)
+    data_accodamento = models.DateTimeField(auto_now_add=True, editable=True)
+    utente = models.ForeignKey(User)
+    luogo = models.TextField(max_length=30)
 
-	class Meta:
-		verbose_name_plural = "Coda Presenze"
-		ordering = ["data_accodamento"]
-		permissions = (
-			('view', 'Visualizzazione coda'),
-		    ('editall', 'Coda di tutti'),
-		)
+    class Meta:
+        verbose_name_plural = "Coda Presenze"
+        ordering = ["data_accodamento"]
+        permissions = (
+            ('view', 'Visualizzazione coda'),
+            ('editall', 'Coda di tutti'),
+        )
 
-	def __unicode__(self):
-		return "%s %s a %s" % (self.data_accodamento.strftime("%d/%m/%Y %H:%M"), self.utente, self.luogo)
+    def __unicode__(self):
+        return "%s %s a %s" % (
+        self.data_accodamento.strftime("%d/%m/%Y %H:%M"), self.utente, self.luogo)
 
 # l'utente potente potrà rimettere in fondo alla coda e cancellare
