612fff0dcda1ef5e6548aebbf6c23d29d8b66aca
Dario Varotto <dariosky@gmail.com>
2016-05-15 22:26:56 +0200

    Multi-device check don't apply to hotel user

--- tam/views/login.py
+++ tam/views/login.py
@@ -18,26 +18,31 @@ class AuthenticationFormWrapped(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
         super(AuthenticationFormWrapped, self).__init__(request, *args, **kwargs)
         self.error_messages['multisession_forbidden'] = _(
-            "Multiple devices connected with the same credentials."
-            "You should disconnect from other devices to connect from here.")
+            u"Più dispositivi sono connessi con le stesse credenziali. "
+            u"Devi disconnetterti per collegarti qua."
+        )
 
     def clean(self):
         cleaned_data = super(AuthenticationFormWrapped, self).clean()
         username = self.cleaned_data.get('username')
         request = self.request
         user = self.user_cache
-        if (settings.FORCE_SINGLE_DEVICE_SESSION and user and
-                not user.has_perm('tam.reset_sessions')):
-            concurrent_sessions = get_concurrent_sessions(request, user)
-            if concurrent_sessions:
-                logger.warning("We have concurrent sessions: login forbidden")
-                logAction('L', description='Accesso proibito - multisessione',
-                          user=user)
-                raise forms.ValidationError(
-                    self.error_messages['multisession_forbidden'],
-                    code='multisession_forbidden',
-                    params={'username': username},
-                )
+        if settings.FORCE_SINGLE_DEVICE_SESSION and user:
+            if hasattr(user, "prenotazioni"):
+                utentePrenotazioni = user.prenotazioni
+            else:
+                utentePrenotazioni = None
+            if not user.has_perm('tam.reset_sessions') and not utentePrenotazioni:
+                concurrent_sessions = get_concurrent_sessions(request, user)
+                if concurrent_sessions:
+                    logger.warning("We have concurrent sessions: login forbidden")
+                    logAction('L', description='Accesso proibito - multisessione',
+                              user=user)
+                    raise forms.ValidationError(
+                        self.error_messages['multisession_forbidden'],
+                        code='multisession_forbidden',
+                        params={'username': username},
+                    )
         return cleaned_data
 
 
