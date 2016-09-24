dcd3e15b72bdbe64bb5c1a470a15ccc17868010f
Dario Varotto <dariosky@gmail.com>
2016-09-24 23:35:58 +0200

    Fix: typo in validator

--- settings.py
+++ settings.py
@@ -462,12 +462,12 @@ PASSWORD_HASHERS = [
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityVal8dator'},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
      'OPTIONS': {'min_length': 8}
      },
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 # END OF DEFAULTS **************************************************************
