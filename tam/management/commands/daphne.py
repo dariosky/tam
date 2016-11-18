512be7c096a94c69e20838215331fc9524f25912
Dario Varotto <dariosky@gmail.com>
2016-11-19 00:07:28 +0100

    Start - stop Daphne works

--- tam/management/commands/daphne.py
+++ tam/management/commands/daphne.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import logging
 import os
+import signal
 import subprocess
 
 import psutil as psutil
@@ -8,7 +9,7 @@ from django.core.management.base import AppCommand
 
 import settings
 
-logging.basicConfig()
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger('tam.deploy')
 
 
@@ -26,11 +27,21 @@ def is_ps_running(pid_file):
         pid = None
     if pid:
         try:
-            p = psutil.Process(pid)
-            print("Process is ", p.status())
-            return True
+            psutil.Process(pid)
+            return pid
         except psutil.NoSuchProcess:
             logger.info("Process in PID not found")
+
+
+def stop_process(pid):
+    try:
+        procs = [psutil.Process(pid)]
+        os.killpg(os.getpgid(pid), signal.SIGTERM)
+        gone, alive = psutil.wait_procs(procs, timeout=5)
+        for p in alive:
+            p.kill()
+    except psutil.NoSuchProcess:
+        logger.info("Process in PID not found")
 
 
 class Command(AppCommand):
@@ -45,17 +56,30 @@ class Command(AppCommand):
         pid_file = settings.DEPLOYMENT['GUNICORN']['PID_FILE']
         log_file = settings.DEPLOYMENT['GUNICORN']['LOG_FILE']
         port = settings.DEPLOYMENT['GUNICORN']['PORT']
+        venv = settings.DEPLOYMENT['FOLDERS']['VENV_FOLDER']
 
         if options['action'] == 'start':
-            print("Checking daphne status %s" % pid_file)
+            logger.debug("Checking daphne status %s" % pid_file)
             running = is_ps_running(pid_file)
             if not running:
                 logger.info("Starting Daphne frontend server")
-                command = ['daphne', '-p %d' % port, 'asgi:channel_layer' '>> %s' % log_file, '2>&1']
-                proc = subprocess.Popen(command, shell=True,
+                command = ['%s/bin/daphne' % venv, '-p %d' % port, 'asgi:channel_layer',
+                           '>> %s' % log_file,
+                           '2>&1'
+                           ]
+                proc = subprocess.Popen(' '.join(command), shell=True,
                                         stdin=None, stdout=None, stderr=None
                                         )
                 with open(pid_file, 'w') as f:
                     f.write("%s\n" % proc.pid)
             else:
-                logger.info("Daphne is running")
+                logger.info("Daphne was already running")
+
+        if options['action'] == 'stop':
+            pid = is_ps_running(pid_file)
+            if pid:
+                logger.info("Stopping Daphne")
+                stop_process(pid)
+                os.remove(pid_file)
+            else:
+                logger.info("Daphne is not running")
