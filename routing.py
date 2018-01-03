47b3a8d4c339212e27eed4ff371a82ad9d51f0c8
Dario Varotto <dariosky@gmail.com>
2018-01-03 10:07:08 +0100

    RTMap: Conditional import of routes

--- routing.py
+++ routing.py
@@ -1,12 +1,14 @@
 # coding=utf-8
 from channels.routing import route_class, route
+from django.conf import settings
 
-from rtmap.consumer import MapConsumer
+channel_routing = []
+if 'rtmap' in settings.PLUGGABLE_APPS:
+    from rtmap.consumer import MapConsumer
 
-channel_routing = [
-    # route("http.request", "tam.consumer.http_consumer"),
-    # route("websocket.receive", ws_message),
-    route_class(MapConsumer),
-
+    channel_routing.append(
+        route_class(MapConsumer)
+    )
+channel_routing.append(
     route('tam.quickbook', 'bots.quickbook.consumer')
-]
+)
